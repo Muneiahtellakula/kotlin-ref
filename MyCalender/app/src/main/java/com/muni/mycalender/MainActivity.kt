1691d25f9a397e7d6f382ee36c7fa6c32e4d16d0
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-04-09 09:11:20 +0530

    Event Implicit Intent assignment Commit

--- MyCalender/app/src/main/java/com/muni/mycalender/MainActivity.kt
+++ MyCalender/app/src/main/java/com/muni/mycalender/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.muni.mycalender
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CalendarView
@@ -7,6 +8,7 @@ import android.widget.DatePicker
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.muni.mycalender.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 lateinit var databinding:ActivityMainBinding
@@ -19,6 +21,22 @@ lateinit var databinding:ActivityMainBinding
             Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
           databinding.textView.text=msg
         }
+        databinding.eventBtn.setOnClickListener { getEvent() }
 
+    }
+
+    private fun getEvent() {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+      //  val event =findViewById<EditText>(R.id.event)
+        val c : Calendar = Calendar.getInstance()
+        val intent = Intent(Intent.ACTION_EDIT)
+        intent.setType("vnd.android.cursor.item/event")
+        intent.putExtra("startTime",c.timeInMillis)
+        intent.putExtra("endTime",c.timeInMillis+60+60+1000)
+        /*For Add Event Static*/
+        //intent.putExtra("title","Birthday")
+        /*For Add Event Dynamically*/
+        intent.putExtra("title",databinding.event.text.toString())
+        startActivity(intent)
     }
 }
