f6a13bf0a30f1040e27fc89900e9594f686ec3ec
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-04-15 21:34:58 +0530

    assignment 5 tired

--- Assignment5/app/src/main/java/com/example/assignment5/MainActivity.kt
+++ Assignment5/app/src/main/java/com/example/assignment5/MainActivity.kt
@@ -2,12 +2,43 @@ package com.example.assignment5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.assignment5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var databinding: ActivityMainBinding
+    var words = ""
+    var mAlphaArray : MutableList<String> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        databinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        databinding.buttonUpdate.setOnClickListener {
+            mAlphaArray  = mutableListOf(
+                "A",
+                "B",
+                "C",
+                "D",
+                "E",
+                "F",
+                "G",
+                "H",
+                "I",
+                "J"
+            )
+            selectWordS()
+
+            databinding.textViewValue.text=words.toString()
+
+        }
+
+    }
+
+    fun selectWordS() {
+        if (mAlphaArray.isEmpty()) {
+            words = "ALL WORDS ARE COMPLETE"
+        } else {
+            words = mAlphaArray.removeAt(0)
+        }
     }
 }
