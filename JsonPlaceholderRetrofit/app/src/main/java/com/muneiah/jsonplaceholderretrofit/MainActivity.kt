fe8d094b97f29e4dadd3d8009bf7a9c73d41a770
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-05-01 13:59:45 +0530

    Retrofit_Coroutines probelm resolved

--- JsonPlaceholderRetrofit/app/src/main/java/com/muneiah/jsonplaceholderretrofit/MainActivity.kt
+++ JsonPlaceholderRetrofit/app/src/main/java/com/muneiah/jsonplaceholderretrofit/MainActivity.kt
@@ -2,16 +2,18 @@ package com.muneiah.jsonplaceholderretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
@@ -20,70 +22,84 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity(), Callback<List<Post>> {
-lateinit var tv:TextView
-    lateinit var post_btn:Button
-    lateinit var get_btn:Button
+class MainActivity() : AppCompatActivity(){
+
+    lateinit var tv : TextView
+    lateinit var getBtn : Button
+    lateinit var postBtn : Button
+
+    constructor(parcel: Parcel) : this() {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       post_btn=findViewById(R.id.button_post)
-        val okhttp=OkHttpClient.Builder()
-            .connectTimeout(1,TimeUnit.MINUTES)
-            .readTimeout(30,TimeUnit.SECONDS)
-            .writeTimeout(30,TimeUnit.SECONDS)
-            .build()
-        get_btn=findViewById(R.id.button_get)
-        tv=findViewById(R.id.respons)
-        val moshi=Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-        val retrofit=Retrofit.Builder()
-        .baseUrl("https://jsonplaceholder.typicode.com/")
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .client(okhttp)
-            .build()
-        val jsonPlaceHoldserService: JsonPlaceHoldserService = retrofit.create(JsonPlaceHoldserService::class.java)
-       get_btn.setOnClickListener {
-           CoroutineScope(Dispatchers.IO).launch {
-               jsonPlaceHoldserService.getAllValues().enqueue(object :Callback<Post>{
 
-               })
-           }
+        /*
+        * By Default, the retrofit library will only wait for 10 seconds
+        * to establish a connection, to fetch data or to write data*/
 
-       }
-        post_btn.setOnClickListener {
-            val post=Post(body = "my body",title = "my titile")
-            jsonPlaceHoldserService.postValue(Post()).enqueue(object :Callback<Post>{
-                override fun onFailure(call: Call<Post>, t: Throwable) {
-                    Log.i("MainActivity","faild")
+        tv = findViewById(R.id.respons)
+        getBtn = findViewById(R.id.button_get)
+        postBtn = findViewById(R.id.button_post)
 
+        /*To Increase the Time */
+
+
+        getBtn.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val service = instance.jsonPlaceHolderInstance()
+                val response = service.getAllValues()
+                if(response.isSuccessful){
+                    withContext(Dispatchers.Main){
+                        val l = response.body()
+                        tv.text = ""
+                        for(i in l!!){
+                            tv.append(i.body+"\n")
+                        }
+                    }
                 }
 
-                override fun onResponse(call: Call<Post>, response: Response<Post>) {
-                    Toast.makeText(this@MainActivity,"success",Toast.LENGTH_SHORT).show()
-                    tv.text=""
-                    val post=response.body()
-                    tv.append(" "+response.code()+"\n"+post?.id+"\n"+
-                    post?.body+"\n"+post?.title)
-                }
-
-            })
+            }
         }
 
-
-    }
-
-    override fun onFailure(call: Call<List<Post>>, t: Throwable) {
-        Log.i("MainActivity",t.message)
-    }
-
-    override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
-    Toast.makeText(this,"success",Toast.LENGTH_SHORT).show()
-        tv.text = ""
-        if (response.isSuccessful) {
-            val list = response.body()
-            for (i in list!!) {
-                tv.append(i.title +i.body+ "\n\n")
+        postBtn.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val service = instance.jsonPlaceHolderInstance()
+                val post = Post(body = "My Body",title = "My Title",userId = 90)
+                val response = service.postValue(post)
+                if(response.isSuccessful){
+                    withContext(Dispatchers.Main){
+                        val post = response.body()
+                        tv.text =""
+                        tv.append(""+post?.id)
+                    }
+                }
             }
+        }
+
+    }
+
+    object instance{
+        fun jsonPlaceHolderInstance(): JsonPlaceHoldserService{
+            val okhttp = OkHttpClient.Builder()
+                .connectTimeout(1,TimeUnit.MINUTES)
+                .readTimeout(30,TimeUnit.SECONDS)
+                .writeTimeout(60,TimeUnit.SECONDS)
+                .build()
+
+
+            val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .client(okhttp)
+                .build()
+
+            val jsonPlaceHolderService : JsonPlaceHoldserService = retrofit.create(JsonPlaceHoldserService::class.java)
+
+            return jsonPlaceHolderService
         }
     }
 }
